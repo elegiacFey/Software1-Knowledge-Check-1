185904ea06d136c811ff213fd01206a4ca9a77b9
Maya <maya@mayas-mbp.lan>
2023-08-11 20:32:53 -0400

    knowledge check submission

---
+++ KnowledgeCheck1_Calculator/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace KnowledgeCheck1_Calculator
+{
+	public class Entry
+	{
+		public (string, string) NumberEntry(string operatorChoice)
+		{
+            Console.WriteLine("Enter 2 integers to " + operatorChoice);
+            string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+            return (number1, number2);
+        }
+	}
+}
+
---
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
+
+            var input = Console.ReadLine();
+            var calculator = new Calculator();
+            var entry = new Entry();
+
+            switch (input)
+            {
+                case "1":
+                    {
+                        string operation = "add";
+                        var (num1, num2) = entry.NumberEntry(operation);
+
+                        if (int.TryParse(num1, out int addNumOne) && int.TryParse(num2, out int addNumTwo))
+                        {
+                            Console.Write($"{num2} + {num2} = ");
+                            Console.Write(calculator.Add(addNumOne, addNumTwo));
+                        }
+                        else
+                        {
+                            Console.WriteLine("One or more of the numbers is not an int");
+                        }
+                        break;
+                    }
+
+                case "2":
+                    {
+                        string operation = "subtract";
+                        var (num1, num2) = entry.NumberEntry(operation);
+
+                        if (int.TryParse(num1, out int subNumOne) && int.TryParse(num2, out int subNumTwo))
+                        {
+                            Console.Write($"{num1} - {num2} = ");
+                            Console.Write(calculator.Subtract(subNumOne, subNumTwo));
+                        }
+                        else
+                        {
+                            Console.WriteLine("One or more of the numbers is not an int");
+                        }
+                        break;
+                    }
+
+                case "3":
+                    {
+                        string operation = "multiply";
+                        var (num1, num2) = entry.NumberEntry(operation);
+
+                        if (int.TryParse(num1, out int multNumOne) && int.TryParse(num2, out int multNumTwo))
+                        {
+                            Console.Write($"{num1} * {num2} = ");
+                            Console.Write(calculator.Multiply(multNumOne, multNumTwo));
+                        }
+                        else
+                        {
+                            Console.WriteLine("One or more of the numbers is not an int");
+                        }
+                        break;
+                    }
+
+                case "4":
+                    {
+                        string operation = "divide";
+                        var (num1, num2) = entry.NumberEntry(operation);
+
+                        if (double.TryParse(num1, out double divNumOne) && double.TryParse(num2, out double divNumTwo))
+                        {
+                            Console.Write($"{num1} / {num2} = ");
+                            Console.Write(calculator.Divide(divNumOne, divNumTwo));
+                        }
+                        else
+                        {
+                            Console.WriteLine("One or more of the numbers is not an int");
+                        }
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("Unknown input");
+                    break;
+            }
+        }
+    }
+}
+
+
